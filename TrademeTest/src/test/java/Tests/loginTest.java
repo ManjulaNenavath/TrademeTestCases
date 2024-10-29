bf289d3932019fd36690c41d2db04b9fa94ccabf
manjula <manjula.nenavath@.com>
2024-10-30 12:49:51 +1300

    added Webdriver code and lauch url

--- TrademeTest/src/test/java/Tests/loginTest.java
+++ TrademeTest/src/test/java/Tests/loginTest.java
@@ -1,17 +1,26 @@
 package Tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class loginTest {
+	WebDriver driver;
 	@BeforeMethod
 	public void setUP() {
+		 driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+		 driver.get("https://www.trademe.co.nz/a/");
 		
 	}
 	@AfterMethod
 	public void TearDown() {
-		
+		driver.quit();
 	}
 	
 	@Test
