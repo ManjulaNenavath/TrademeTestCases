a02506154942ede65d46f8828e19822a287424d8
manjula <manjula.nenavath@.com>
2024-10-30 08:50:58 +1300

    Created basic folder structure for pom

---
+++ TrademeTest/src/test/java/Tests/loginTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class loginTest {
+	@BeforeMethod
+	public void setUP() {
+		
+	}
+	@AfterMethod
+	public void TearDown() {
+		
+	}
+	
+	@Test
+	public void LoginWithValidCredentials() {
+		
+	}
+
+}
---
+++ TrademeTest/src/test/java/pages/loginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class loginPage {
+	WebDriver driver;
+	public loginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+
+}
